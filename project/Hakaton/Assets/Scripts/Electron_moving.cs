020b6053343c53b9b699103df61d9baf3180cf1d
Kyrylo <kirill26031@gmail.com>
2019-04-13 18:19:15 +0300

    Hydrohenium move

---
+++ project/Hakaton/Assets/Scripts/Electron_moving.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Electron_moving : MonoBehaviour
+{
+    public float radius;
+    public float speed;
+    float angle;
+    Vector3 axis = new Vector3(0,1,0);
+    Quaternion rotateQuaternion;
+    void Start()
+     {
+        angle = 180f*speed/radius/3.14159f;
+        transform.position = new Vector3(-radius, 0, 0);
+        rotateQuaternion = Quaternion.AngleAxis(angle, axis);
+    }
+    void FixedUpdate()
+    {
+        transform.rotation*=rotateQuaternion;
+        transform.position+=transform.forward*speed;
+    }
+}
