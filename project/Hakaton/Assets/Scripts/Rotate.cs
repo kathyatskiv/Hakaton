84e4a03d4ea0f6865eb9ace2478251bf2d48ea6a
black_heart <43214283+LesYer@users.noreply.github.com>
2019-04-14 03:13:49 +0300

    new molecus

---
+++ project/Hakaton/Assets/Scripts/Rotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    Quaternion originRotation;
+    float angle;
+    void Start()
+    {
+        originRotation = transform.rotation;
+    }
+
+    
+    void FixedUpdate ()
+    {
+        angle++;
+        Quaternion rotationY = Quaternion.AngleAxis(angle, Vector3.up);
+        //Quaternion rotationX = Quaternion.AngleAxis(angle, Vector3.right);
+
+        transform.rotation = originRotation * rotationY;
+    }
+}
